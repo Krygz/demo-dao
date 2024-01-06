8a7a42dece6eb4d5dba8f43cd3f4282b05a7da92
Pedro <ph4290245@gmail.com>
2024-01-06 13:53:43 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,19 @@
 package application;
 
+
+import java.sql.Date;
+
 import models.entities.Department;
+import models.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
 	
 	Department department = new Department(2 , "Jhon");
-	System.out.print(department);
+
+	Seller seller = new Seller(21 , "bob" , "bob@gmail.com" ,new Date(0),  3000.00 , department);
+	
+	System.out.print(seller);
+	
 	}
 }
